f97a3c573167d5f32308ae053991136f815e5dc5
Joaquin <joakobsc@gmail.com>
2024-11-12 14:25:53 -0300

    actualizando  views

--- src/routes/views.Router.js
+++ src/routes/views.Router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import ProductManager from "../service/ProductManager.js"; // Importa ProductManager
 import { productsModel } from "../models/product.model.js";
+import { cartModel } from "../models/cart.model.js";
 const router = Router();
 
 // Crea una instancia de ProductManager
@@ -16,31 +17,31 @@ const productManager = new ProductManager();
 
 router.get("/products", async (req, res) => {
   try {
+    // Crea un nuevo carrito vacío y guárdalo en la base de datos
+    const newCart = new cartModel({ products: [] });
+    await newCart.save(); // Guardamos el carrito en la base de datos
+    console.log("Nuevo cartId:", newCart._id); // Asegúrate de que esta línea imprima el ID correctamente
+
     // Extraemos la página actual y la cantidad de productos por página
     const page = parseInt(req.query.page) || 1;
     const limit = 5; // Número de productos por página
-
-    // Calcular el salto para la paginación
     const skip = (page - 1) * limit;
 
     // Obtener productos con paginación
     const products = await productsModel.find().skip(skip).limit(limit);
-
-    // Obtener el total de productos para calcular el número de páginas
     const totalProducts = await productsModel.countDocuments();
-
-    // Calcular el número total de páginas
     const totalPages = Math.ceil(totalProducts / limit);
 
-    // Renderizar la vista 'index.handlebars' con los productos y la paginación
+    // Renderizar la vista 'home' con los productos, carrito y paginación
     res.render("home", {
       products,
+      cart: newCart, // Pasas el carrito completo
       currentPage: page,
       totalPages,
       style: "index.css",
     });
   } catch (error) {
-    console.log("Error al obtener productos:", error); // Agrega esta línea para ver el error detallado
+    console.log("Error al obtener productos:", error);
     res.status(500).json({ error: "Error al obtener productos" });
   }
 });
